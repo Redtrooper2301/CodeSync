c2758b02a7445a974fd7db77367923cc91314bc8
Aniket Chouhan <aniketchouhan2705@gmail.com>
2025-03-26 22:04:46 +0530

    language bar added

--- my-vite-app/src/Components/CodeEditor.jsx
+++ my-vite-app/src/Components/CodeEditor.jsx
@@ -9,10 +9,25 @@ import "/src/App.css";
 const CodeEditor = () => {
   const [code, setCode] = useState("// Start coding...");
   const [theme, setTheme] = useState("vs-dark");
+  const [language, setLanguage] = useState("javascript");
 
   return (
     <div className="editor-container">
       <h2>🔥 Online Code Editor</h2>
+
+      <div>
+      <select onChange={(e) => setLanguage(e.target.value)} value={language}>
+        <option value="javascript">JavaScript</option>
+        <option value="python">Python</option>
+        <option value="cpp">C++</option>
+        <option value="java">Java</option>
+        <option value="C">C</option>
+        <option value="C#">C#</option>
+        
+      </select>
+
+      
+    </div>
 
       {/* Theme Toggle Button */}
       <button className="theme-toggle" onClick={() => setTheme(theme === "vs-dark" ? "light" : "vs-dark")}>
